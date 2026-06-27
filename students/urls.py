c4b1813121d44416a965911918921dfa19468099
venkataraghavendraraochatti <cvrr27894@gmail.com>
2026-06-27 19:07:56 +0530

    django template language(DTL) - Filters, if-elif-else.

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 #path() is used to map a URL pattern to a view.
 from . import views
-#from . import views
 
 urlpatterns = [
     path("home/", views.home),
     path("<int:id>/", views.student_details),
+    path("context-filters/<int:id>/", views.student_details_filters_context),
+    path("profile/", views.student_profile),
 ]
--- students/views.py
+++ students/views.py
@@ -14,3 +14,21 @@ def student_details(request, id):
         "duration": "30 Days",
     }
     return render(request, "students/context-student.html", context)
+
+def student_details_filters_context(request, id):
+    context = {
+        "id": id,
+        "name": "raghavendra rao",
+        "course": "django web development",
+        "students": ["Ram", "Krishna", "Sai", "Anil"]
+    }
+    return render(request, "students/context-filter-student.html", context)
+
+def student_profile(request):
+    context = {
+        "name": "Raghavendra",
+        "age": 28,
+        "marks": 92,
+        "is_placed": False
+    }
+    return render(request, "students/student-profile.html", context)
