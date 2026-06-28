db03b1295cbdcfe2129dfee865079f07963bd099
venkataraghavendraraochatti <cvrr27894@gmail.com>
2026-06-28 20:52:29 +0530

    django relationships.

--- students/admin.py
+++ students/admin.py
@@ -1,7 +1,8 @@
 
 # Register your models here.
 from django.contrib import admin
-from .models import Student
+from .models import Student, StudentFK, Course
+
 
 # admin.site.register(Student)
 
@@ -31,3 +32,42 @@ class StudentAdmin(admin.ModelAdmin):
     ordering = (
         "-id",
     )
+
+@admin.register(StudentFK)
+class StudentFKAdmin(admin.ModelAdmin):
+
+    #list_display used to display the columns in the grid
+    list_display = (
+        "id",
+        "name",
+        "age",
+        "course",
+    )
+
+    #search_fields adds up a search bar, upon entering a value, it searhes in the defined columns only
+    search_fields = (
+        "name",
+        "course",
+    )
+
+    #we can filter out the records with all distinct values of course column
+    list_filter = (
+        "course",
+    )
+
+    #only sorting is made available on id column, by default its desc - means desc
+    ordering = (
+        "-id",
+    )
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+
+    #list_display used to display the columns in the grid
+    list_display = (
+        "id",
+        "name",
+        "duration",
+        "fee",
+    )
+
--- students/models.py
+++ students/models.py
@@ -1,9 +1,30 @@
 from django.db import models
 
+#basic model
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     course = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    fee = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+#one to many relationship
+class StudentFK(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
