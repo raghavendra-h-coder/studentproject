fad460b6714e41d1489b34de22af74cb81367e92
venkataraghavendraraochatti <cvrr27894@gmail.com>
2026-06-28 19:55:47 +0530

    django admin.

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Student
+
+# admin.site.register(Student)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+
+    #list_display used to display the columns in the grid
+    list_display = (
+        "id",
+        "name",
+        "age",
+        "course",
+    )
+
+    #search_fields adds up a search bar, upon entering a value, it searhes in the defined columns only
+    search_fields = (
+        "name",
+        "course",
+    )
+
+    #we can filter out the records with all distinct values of course column
+    list_filter = (
+        "course",
+    )
+
+    #only sorting is made available on id column, by default its desc - means desc
+    ordering = (
+        "-id",
+    )
